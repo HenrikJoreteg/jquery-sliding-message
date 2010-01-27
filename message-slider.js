cf605b628e6d7d07d267dfab17372ddef8077e15
Henrik Joreteg <Henrik@Henriks-Mac.local>
2010-01-26 21:45:32 -0800

    cleaning up demo and adding more options

--- message-slider.js
+++ message-slider.js
@@ -4,25 +4,29 @@
         settings = jQuery.extend({
              id: 'message_box',
              position: 'bottom',
-             size: '90px',
+             size: '90',
              backgroundColor: 'rgb(143, 177, 240)',
              delay: 1500,
-             speed: 500
+             speed: 500,
+             fontSize: '30px'
         }, options);        
         
         var elem = $('#' + settings.id);
         var delayed;
         
-        console.log($(settings.id));
-
         // generate message div if it doesn't exist
         if(elem.length == 0){
             elem = $('<div></div>').attr('id', settings.id);
             
-            elem.css({'left': '0', 
-                      'z-index': '999',
+            elem.css({'z-index': '999',
                       'background-color': settings.backgroundColor,
-                      'text-align': 'center'
+                      'text-align': 'center',
+                      'position': 'fixed',
+                      'bottom': '-' + settings.size + 'px',
+                      'left': '0',
+                      'width': '100%',
+                      'line-height': settings.size + 'px',
+                      'font-size': settings.fontSize,
                       });
             
             $('body').append(elem);
@@ -32,8 +36,11 @@
         
         if(settings.position == 'bottom'){
             elem.animate({bottom:'0'}, settings.speed);
-            delayed = "$(" + settings.id + ").animate({bottom:'-90px'}, " + settings.speed + ");";
+            delayed = '$(' + settings.id + ').animate({bottom:"-' + settings.size + 'px"}, ' + settings.speed + ');';
             setTimeout(delayed, settings.delay);
+        }
+        else if(settings.position == 'top'){
+            
         }
         
     }
